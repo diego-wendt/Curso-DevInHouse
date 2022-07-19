9125602a0b06aa1e67a505ada2cf898daabf5792
diego-wendt <diego_wendt@hotmail.com>
2022-07-18 22:58:45 -0300

    [M1S03]Ex04
    Resolução Exercício
    Módulo 1 - Semana 3 - Exercício 04

---
+++ Exercícios/Módulo1/Semana3/Ex04/index.js
@@ -0,0 +1,41 @@
+var convidado1 = {
+    nome: "Naruto",
+    sobrenome: "Uzumaki",
+    setor: "pista",
+    idade: 15,
+}
+
+var convidado2 = {
+    nome: "Sasuke",
+    sobrenome: "Uchiha",
+    setor: "arquibancada",
+    idade: 16,
+}
+
+var convidado3 = {
+    nome: "Goku",
+    sobrenome: "Sayajin",
+    setor: "arquibancada",
+    idade: 20,
+}
+
+var convidado4 = {
+    nome: "Vegeta",
+    sobrenome: "Sayajin",
+    setor: "camarote",
+    idade: 18,
+}
+
+var convidado5 = {
+    nome: "Sakura",
+    sobrenome: "Haruno",
+    setor: "pista",
+    idade: 19,
+}
+
+var convidado6 = {
+    nome: "Hinata",
+    sobrenome: "Hyuga",
+    setor: "camarote",
+    idade: 16,
+}
