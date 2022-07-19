aafaf9f8ad14bdc7e34fc11e23b9f1f3603bbc19
diego-wendt <diego_wendt@hotmail.com>
2022-07-18 23:00:11 -0300

    [M1S03]Ex05
    Resolução Exercício
    Módulo 1 - Semana 3 - Exercício 05

---
+++ Exercícios/Módulo1/Semana3/Ex05/index.js
@@ -0,0 +1,47 @@
+var convidado1 = {
+    nome: "Naruto",
+    sobrenome: "Uzumaki",
+    setor: "pista",
+    idade: 15,
+}
+
+var convidado2 = {
+    nome: "Sasuke",
+    sobrenome: "Uchiha",
+    setor: "arquibancada",
+    idade: 16,
+}
+
+var convidado3 = {
+    nome: "Goku",
+    sobrenome: "Sayajin",
+    setor: "arquibancada",
+    idade: 20,
+}
+
+var convidado4 = {
+    nome: "Vegeta",
+    sobrenome: "Sayajin",
+    setor: "camarote",
+    idade: 18,
+}
+
+var convidado5 = {
+    nome: "Sakura",
+    sobrenome: "Haruno",
+    setor: "pista",
+    idade: 19,
+}
+
+var convidado6 = {
+    nome: "Hinata",
+    sobrenome: "Hyuga",
+    setor: "camarote",
+    idade: 16,
+}
+
+var seMaiorIdade = { openbar: false }
+
+var listaConvidados = [convidado1, convidado2, convidado3, convidado4, convidado5, convidado6];
+
+console.log(listaConvidados)
