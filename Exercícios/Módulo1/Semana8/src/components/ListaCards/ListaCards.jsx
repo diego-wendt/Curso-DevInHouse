e8a04939e3cc961caafa494356620a31004f005e
diego-wendt <diego_wendt@hotmail.com>
2022-08-22 21:07:59 -0300

    [M1S08]Ex05

--- Exercícios/Módulo1/Semana8/src/components/ListaCards/ListaCards.jsx
+++ Exercícios/Módulo1/Semana8/src/components/ListaCards/ListaCards.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
-
 import { Card } from '@components';
-
 import styles from './ListaCards.module.css';
+import { useState } from 'react';
 
-export const ListaCards = ({ produtos }) => (
+export const ListaCards = ({ produtos }) => {
+  const [idSelecionados, setIdSelecionados]= useState([]);
+  
+  return (
   <ul className={styles.lista}>
     {produtos.map((produto) => (
       <li className={styles.listaItem} key={produto.id}>
@@ -12,7 +14,7 @@ export const ListaCards = ({ produtos }) => (
       </li>
     ))}
   </ul>
-);
+);}
 
 ListaCards.propTypes = {
   produtos: PropTypes.array.isRequired,
