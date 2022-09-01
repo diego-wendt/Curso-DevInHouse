d2cc3486253235879be2cd82e28631aa9a1beac0
diego-wendt <diego_wendt@hotmail.com>
2022-09-01 00:08:45 -0300

    [M1S09]Ex09
    Resolução Exercício
    Módulo 1 - Semana 9 - Exercício 09

--- Exercícios/Módulo1/Semana9/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ Exercícios/Módulo1/Semana9/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -1,18 +1,21 @@
+import { ListaCards } from "@components";
+import { useProdutosSelecionados } from "@contexts";
 
-import { ListaCards } from "@components"
-import { useProdutosSelecionados } from "@contexts"
+export const ProdutosSelecionados = () => {
+  const { produtosSelecionados } = useProdutosSelecionados();
 
-export const ProdutosSelecionados = () =>{
-    
-    const {produtosSelecionados} = useProdutosSelecionados()
-    
-    return (
-        <>
-        
+  const total = produtosSelecionados.reduce((soma, item) => {
+    return soma + item.valor;
+  }, 0);
+
+  return (
+    <>
+      <div>
         <h1>Produtos Selecionados</h1>
+        <h2>{`R$ ${total.toFixed(2).replace(".", `,`)}`}</h2>
+      </div>
 
-        <ListaCards produtos={produtosSelecionados}/>
-
-        </>
-    )
-}
+      <ListaCards produtos={produtosSelecionados} />
+    </>
+  );
+};
