b1441b1c55950cce7f11d3822f2ea87af870d013
diego-wendt <diego_wendt@hotmail.com>
2022-09-07 20:15:01 -0300

    [M1S10]Ex09
    Resolução Exercício
    Módulo 1 - Semana 10 - Exercício 09

--- Exercícios/Módulo1/Semana10/src/contexts/app-context.jsx
+++ Exercícios/Módulo1/Semana10/src/contexts/app-context.jsx
@@ -4,41 +4,51 @@ export const appContext = React.createContext(null);
 
 const ContextProvider = appContext.Provider;
 
-
 // este componente é quem vai permitir que os elementos abraçados tenham acesso aos dados que estão dentro do value do ContextProvider
 export const AppContextProvider = ({ children }) => {
- 
-  const [allTips, setAllTips ] = useState([
+  const [allTips, setAllTips] = useState([
     {
       titulo: "aaaa",
       linguagem: "mais um teste",
       categoria: "devOps",
-      descricao: "lorem10 lorem10 lorem10 lorem10lorem10 lorem10 lorem10 lorem10",
+      descricao:
+        "lorem10 lorem10 lorem10 lorem10lorem10 lorem10 lorem10 lorem10",
       video: "http://www.globo.com",
     },
-  ])
-  const [filter, setFilter] = useState(null)
- 
+  ]);
+  const [filter, setFilter] = useState(null);
+
   const createTip = (tip) => {
-    setAllTips((previousTips) => [...previousTips, tip])
-  }
-  
+    setAllTips((previousTips) => [...previousTips, tip]);
+  };
 
   const filterTips = (query) => {
-    if (query){
-      setFilter(query)
-    }else{
-      setFilter(null)
+    if (query) {
+      setFilter(query);
+    } else {
+      setFilter(null);
     }
+  };
 
-  }
-  
-  const tips = filter ? allTips.filter(tip=>tip.titulo.includes(filter)): allTips;
- 
+  const tips = filter
+    ? allTips.filter((tip) => tip.titulo.includes(filter))
+    : allTips;
+
+  const sortByCategory = () => {
+    const categories = Array.from(new Set(allTips.map((tip) => tip.categoria)));
+    return categories.map((catName) => {
+      const count = allTips.filter((tip) => tip.categoria == catName).length;
+      return { title: catName, count: count };
+    });
+  };
+
+  const categories = sortByCategory();
+
   return (
-  <ContextProvider value={{createTip, filterTips, tips}}>
-    {children}
-    </ContextProvider>)
+    <ContextProvider value={{ createTip, filterTips, tips, categories }}>
+      {children}
+    </ContextProvider>
+  );
 };
 
 // esse o customHook é chamado em cada local que se deseja ter acesso á informação dentro do Context, e em vez de importar o React.useContext(appContext), importa-se apenas o useAppContext que é o customHook que ja traz todos esses imports embutidos
