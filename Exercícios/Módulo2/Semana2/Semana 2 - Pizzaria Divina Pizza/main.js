c80e6508bd3780f0d3d6425509edd06462f5f187
diego-wendt <diego_wendt@hotmail.com>
2022-10-23 01:43:57 -0300

    [M2S02]Ex02
    Resolução Exercício
    Módulo 2 - Semana 02 - Exercício 02
    Criação de rotas para cadastrar pizzas (post) e para listar pizzas (get)

--- Exercícios/Módulo2/Semana2/Semana 2 - Pizzaria Divina Pizza/main.js
+++ Exercícios/Módulo2/Semana2/Semana 2 - Pizzaria Divina Pizza/main.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
+
 
 const app = express();
+app.use(express.json());
+
+let pizzas = [];
+
+app.get('/pizzas', (request, response)=>{
+    console.log('listou todas as pizzas');
+    response.json(pizzas);
+})
+
+app.post('/pizzas', (request,response)=>{
+
+    const pizza = {
+        id: uuidv4(),
+        name: request.body.name,
+        description: request.body.description,
+        price: request.body.price,
+        ingredients: request.body.description,
+        url: request.body.url 
+    }
+
+    // // OU PODERIA SER ESCRITO COMO:
+    // const {name, description, price, ingredients, url} = request.body
+    // const pizza = {id: uuidv4(), name, description, price, ingredients, url}
+
+    pizzas.push(pizza);
+    response.status(201).json(pizza);
+    console.log('Cadastrou uma pizza');
+})
+
 
 app.listen(3333, ()=>{
     console.log('Servidor Online');
